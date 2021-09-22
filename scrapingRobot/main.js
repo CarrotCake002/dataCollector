af6da7cfd87fa664150e45c20b756235c74b2fef
Pol Siles <pol.siles-polo@epitech.eu>
2021-09-22 11:16:09 +0200

    commented apify code because my windows version doesn't allow me to use it

--- scrapingRobot/main.js
+++ scrapingRobot/main.js
@@ -1,36 +1,37 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const Apify = require('apify');
+/*const Apify = require('apify');
 
-Apify.main(async () => {
-    const requestList = new Apify.RequestList({
-        sources: [{ requestsFromUrl: 'https://edition.cnn.com/sitemaps/cnn/news.xml' }],
-    });
-    await requestList.initialize();
+function getSitemap() {
+    return (Apify.main(async () => {
+        const requestList = new Apify.RequestList({
+            sources: [{ requestsFromUrl: 'https://www.shbarcelona.com/sitemap.xml' }],
+        });
+        await requestList.initialize();
 
-    const requestQueue = await Apify.openRequestQueue();
+        const requestQueue = await Apify.openRequestQueue();
 
-    const crawler = new Apify.PuppeteerCrawler({
-        requestList,
-        requestQueue,
-        handlePageFunction: async ({ page, request }) => {
-            console.log(`Processing ${request.url}...`);
+        const crawler = new Apify.PuppeteerCrawler({
+            requestList,
+            requestQueue,
+            handlePageFunction: async ({ page, request }) => {
+                console.log(`Processing ${request.url}...`);
 
-            // This is just an example, define your logic
-            await Apify.utils.enqueueLinks({
-                page, selector: 'a', pseudoUrls: null, requestQueue,
-            });
-            await Apify.pushData({
-                url: request.url,
-                title: await page.title(),
-                html: await page.content(),
-            });
-        },
-    });
-
-    await crawler.run();
-    console.log('Done.');
-});
+                // This is just an example, define your logic
+                await Apify.utils.enqueueLinks({
+                    page, selector: 'a', pseudoUrls: null, requestQueue,
+                });
+                await Apify.pushData({
+                    url: request.url,
+                    title: await page.title(),
+                    html: await page.content(),
+                });
+            },
+        });
+        await crawler.run();
+        console.log('Done.');
+    }));
+}*/
 
 async function readSitemap(linkList) {
     var i = 1;
