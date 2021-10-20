5dfefe8e7c14ae7d7f80f97b03945feb24af3df1
Pol Siles <pol.siles-polo@epitech.eu>
2021-10-20 17:10:29 +0200

    moved the initialisation of defaultParms to a new init.js file now that i know how to import data from other .js files

--- scrapingRobot/main.js
+++ scrapingRobot/main.js
@@ -745,33 +745,6 @@ function configParams(args, defaultParams) {
     return defaultParams;
 }
 
-// set the default values of the program's arguments formatting
-var defaultParams = {
-    domain: null,
-    notEnterLinksWith: ["mailto:", "javascript:", "tel:", "steam:", "#", "excel", "word", "pdf"],
-    onlyEnterLinksWith: null,
-    notSaveLinksWith: null,
-    onlySaveLinksWith: null,
-    savefile: "default",
-    querySelector: ['meta', 'title', 'link', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
-    getOneSelector: false,
-    formattedSavefile: false,
-    headlessBrowser: false,
-    args: process.argv,
-    clickItems: null,
-    sitemapLink: null,
-    startingUrls: null,
-    startingUrlsFile: null,
-    maxDepth: 999,
-    getLinkArticle: false,
-    getMeta: false,
-    getHeads: false,
-    getHreflang: false,
-    getCanonical: false,
-    getTitle: false,
-
-}
-
 // get the program execution arguments
 const args = process.argv.slice(2);
 const params = configParams(args, defaultParams);
