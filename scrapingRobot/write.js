afc8ba0a5c6c1c75cc6fc03436b0b92d0e46dd86
Pol Siles <pol.siles-polo@epitech.eu>
2021-10-21 13:37:32 +0200

    changed the path of the location of the savefile. It will not be sent as an argument but instead will be an unchanged relative path. Since i will make folders for all files about the robot, the path when executing the robot in the website or in terminal directly will be unchanged

--- scrapingRobot/write.js
+++ scrapingRobot/write.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 // function to write a string in the specified file. If the file doesn't exist it will be created
 async function writeInFile(rootPath, string, savefile) {
-    fs.writeFile(rootPath + "/../../savefiles/" + savefile + '.json', string, { flag: 'a+' }, (err) => {
+    fs.writeFile('../../savefiles' + savefile + '.json', string, { flag: 'a+' }, (err) => {
     });
 }
 
