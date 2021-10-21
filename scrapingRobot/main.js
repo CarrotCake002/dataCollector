2c8fdc4b4d75fd684efff0a5c5e450446ab4fbb9
Pol Siles <pol.siles-polo@epitech.eu>
2021-10-21 13:34:53 +0200

    removed all functions related to saving the data in a file

--- scrapingRobot/main.js
+++ scrapingRobot/main.js
@@ -75,69 +75,20 @@ async function getContent(linkList, iList, linkEnteredCount) {
     returnArray = await getContent(linkList, iList, linkEnteredCount);
     linkEnteredCount--;
     if (linkEnteredCount === 0) {
-        saveFinalData(returnArray[0]);
+        save.saveFinalData(returnArray[0], params);
     }
     return returnArray;
 }
 
 
 
-// this function saves the data that was being updated in runtime and that could not be saved in the main file before
-function saveFinalData(linkList) {
-    var saveData = [];
-    var i = 0;
-
-    while (i < linkList.length) {
-        if (i > 0)
-            i = skipLinks(i, linkList, params['notEnterLinksWith'], params['onlyEnterLinksWith'])
-        if (i === false)
-            break;
-        saveData.push(linkList[i][2]);
-        i++;
-    }
-
-    params['formattedSavefile'] ? saveData = JSON.stringify(saveData) : saveData = JSON.stringify(saveData);
-    saveData = '"runtime": ' + saveData + '\n}';
-    write.writeInFile(params['args'][1], saveData, params['savefile']);
-    saveData = null;
-}
-
-// save data that won't be modified and will be deleted in runtime for better optimization
-async function saveFormData(resultArray, iteration, time, linkEnteredCount) {
-    var url = resultArray[0][iteration][0];
-
-    if (resultArray[0][iteration][0].includes('http://') === false && resultArray[0][iteration][0].includes('https://') === false) {
-        url = params['domain'] + url;
-    }
-    var jsonObj =
-    {
-        "Iteration": iteration,
-        "url": url,
-        "status": resultArray[9],
-        "urlDepth": resultArray[0][iteration][1],
-        "time": time / 1000,
-        "html": {
-            "title": resultArray[4],
-            "meta": resultArray[3],
-            "hreflang": resultArray[5],
-            "canonicals": resultArray[6],
-            "heads": resultArray[7],
-            "linkArticle": resultArray[8],
-            "userSelected": resultArray[1],
-        },
-        "links": resultArray[2],
-    };
-    params['formattedSavefile'] ? jsonObj = JSON.stringify(jsonObj, null, 4) : jsonObj = JSON.stringify(jsonObj);
-
-    jsonObj = '"' + linkEnteredCount + '": ' + jsonObj + ',\n\t';
-
-    write.writeInFile(params['args'][1], jsonObj, params['savefile']);
-}
+// --> save here
+const save = require("./save.js");
 
 var eventEmitter = new events.EventEmitter();
 
 var dataHandler = async function (returnArray, iteration, time, linkEnteredCount) {
-    await saveFormData(returnArray, iteration, time, linkEnteredCount);
+    await save.saveFormData(returnArray, iteration, time, linkEnteredCount, params);
 }
 
 eventEmitter.on('saveData', dataHandler);
@@ -154,7 +105,8 @@ logs.any(params);
 if (params['sitemapLink'] === null)
     init.linkList = params['startingUrls'];
 
-write.writeInFile(params['args'][1], '{\n\t', params['savefile']);
+
+write.writeInFile('{\n\t', params['savefile']);
 
 var returnArray = getContent(init.linkList, init.iList, init.linkEnteredCount);
 
