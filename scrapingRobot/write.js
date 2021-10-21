e8bc75f85f0b8ca8f94de67de17f57d2d707bb83
Pol Siles <pol.siles-polo@epitech.eu>
2021-10-21 14:15:37 +0200

    added writeInFile function to write.js and exported it. Also changed the save path as i have moved a lot of files and managed to make a similar path for launchRobot.php and main.js, which were conflicting before. Now executing it from terminal should be the same as executing from website

--- scrapingRobot/write.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-// function to write a string in the specified file. If the file doesn't exist it will be created
-async function writeInFile(rootPath, string, savefile) {
-    fs.writeFile('../../savefiles' + savefile + '.json', string, { flag: 'a+' }, (err) => {
-    });
-}
-
-module.exports = { writeInFile };
