bc3d18b1fc63b57d534e4836dc6023a8fe5dd0f3
Pol Siles <pol.siles-polo@epitech.eu>
2021-10-22 13:26:14 +0200

    created new file getContent.js in main folder which contains the getContent function. It is the main loop function so it deserves a file of its own with a couple functions directly related to it. This file can still be made shorter and more readable so it's just a first version of it

---
+++ scrapingRobot/main/getContent.js
@@ -0,0 +1,43 @@
+// my imported files
+const imports = require("./imports.js");
+
+async function getPageData(linkList, iList, page, response) {
+    await imports.click.clickItems(imports.configStart.params['clickItems']);
+    var returnArray = await imports.eval.evaluate(linkList, imports.configStart.params, iList, page);
+    return (imports.open.checkErrors(returnArray, response.status()) ? returnArray : null);
+}
+
+// main loop of the program. Recursive function that open/closes browsers and gets all the information from every page
+async function getContent(linkList, iList, linkEnteredCount) {
+
+    linkList = await imports.sitemap.check(iList, linkList, imports.configStart.params);
+
+    const formattedLink = imports.link.getFormattedLink(iList, linkList[iList][0], imports.configStart.params['domain']);
+    linkEnteredCount++;
+
+    var openPage = await imports.open.startPage(imports.configStart.params, formattedLink);
+    const [browser, page, response, startTime] = [openPage[0], openPage[1], openPage[2], openPage[3]];
+    openPage = null;
+
+    if (returnArray = await getPageData(linkList, iList, page, response), returnArray === null)
+        return null;
+
+    const endTime = imports.open.endPage(browser, startTime);
+
+    returnArray = returnArray.concat(response.status());
+
+    await imports.save.saveFormData(returnArray, iList, endTime, linkEnteredCount, imports.configStart.params);
+
+    var linkList = returnArray[0];
+
+    if (iList = imports.link.getNext(iList, linkList, imports.configStart.params), iList === true) {
+        imports.save.end(returnArray, linkEnteredCount, imports.configStart.params);
+        return returnArray;
+    }
+
+    returnArray = null;
+    returnArray = await getContent(linkList, iList, linkEnteredCount);
+    return imports.save.end(returnArray, linkEnteredCount, imports.configStart.params);
+}
+
+module.exports = { getContent };
