380bed0bb159ae1e4067eb097fb002f4bd41bff5
Pol Siles <pol.siles-polo@epitech.eu>
2021-10-22 10:22:11 +0200

    added imports.js file which imports all libraries the main.js file need and then exports them in a single object

---
+++ scrapingRobot/main/imports.js
@@ -0,0 +1,12 @@
+const configStart = require("./../init/config.js");
+const sitemap = require("./../page/sitemap.js");
+const eval = require("./../page/evaluate.js");
+const click = require("./../page/click.js");
+const write = require("./../text/write.js");
+const logs = require("./../text/logs.js");
+const link = require("./../page/link.js");
+const open = require("./../page/open.js");
+const init = require("./../init/init.js");
+const save = require("./../data/save.js");
+
+module.exports = { configStart, sitemap, eval, click, write, logs, link, open, init, save };
