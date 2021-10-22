59d5873248ac4fec5c9d1e0d0e993bb8218461ae
Pol Siles <pol.siles-polo@epitech.eu>
2021-10-22 10:24:10 +0200

    deleted all imported modules and added only imports.js. This way i get the same modules but all put together in a single object called imports. This allows me to have only 1 line of imports in main.js and makes it easier to import new modules since i can do it in the newly created file import.js, and t will be automatically added to main.js when exported

--- scrapingRobot/main/main.js
+++ scrapingRobot/main/main.js
@@ -1,70 +1,60 @@
 // my imported files
-const configStart = require("./../init/config.js");
-const sitemap = require("./../page/sitemap.js");
-const eval = require("./../page/evaluate.js");
-const click = require("./../page/click.js");
-const write = require("./../text/write.js");
-const logs = require("./../text/logs.js");
-const link = require("./../page/link.js");
-const open = require("./../page/open.js");
-const init = require("./../init/init.js");
-const save = require("./../data/save.js");
-
+const imports = require("./imports.js");
 
 // main loop of the program. Recursive function that open/closes browsers and gets all the information from every page
 async function getContent(linkList, iList, linkEnteredCount) {
 
-    linkList = await sitemap.check(iList, linkList, params);
+    linkList = await imports.sitemap.check(iList, linkList, params);
 
-    const formattedLink = link.getFormattedLink(iList, linkList[iList][0], params['domain']);
+    const formattedLink = imports.link.getFormattedLink(iList, linkList[iList][0], params['domain']);
     linkEnteredCount++;
 
     // open a new browser and page with the new url
     var time = Date.now();
-    var openPage = await open.page(params, formattedLink)
+    var openPage = await imports.open.page(params, formattedLink)
     const [browser, page, response] = [openPage[0], openPage[1], openPage[2]];
     openPage = null;
 
-    await click.clickItems(params['clickItems']);
-    var returnArray = await eval.evaluate(linkList, params, iList, page);
+    await imports.click.clickItems(params['clickItems']);
+    var returnArray = await imports.eval.evaluate(linkList, params, iList, page);
 
     browser.close();
     time = Date.now() - time;
 
-    if (open.checkErrors(returnArray, response.status()) === false)
+    if (imports.open.checkErrors(returnArray, response.status()) === false)
         return null;
 
     returnArray = returnArray.concat(response.status());
 
-    await save.saveFormData(returnArray, iList, time, linkEnteredCount, params);
+    await imports.save.saveFormData(returnArray, iList, time, linkEnteredCount, params);
 
     var linkList = returnArray[0];
 
-    if (iList = link.getNext(iList, linkList, params), iList === true)
+    if (iList = imports.link.getNext(iList, linkList, params), iList === true)
         return returnArray;
 
     returnArray = null;
     returnArray = await getContent(linkList, iList, linkEnteredCount);
     linkEnteredCount--;
-    return save.end(returnArray, linkEnteredCount, params);
+    return imports.save.end(returnArray, linkEnteredCount, params);
 }
 
 // get the program execution arguments
 const args = process.argv.slice(2);
-const params = configStart.configParams(args, init.defaultParams);
+const params = imports.configStart.configParams(args, imports.init.defaultParams);
 if (!params) {
-    logs.options();
+    imports.logs.options();
     return 84;
 }
-logs.any(params);
+imports.logs.any(params);
 if (params['sitemapLink'] === null)
-    init.linkList = params['startingUrls'];
+    imports.init.linkList = params['startingUrls'];
 
 
-write.writeInFile('{\n\t', params['savefile']);
+imports.write.writeInFile('{\n\t', params['savefile']);
 
 
-getContent(init.linkList, init.iList, init.linkEnteredCount);
+getContent(imports.init.linkList, imports.init.iList, imports.init.linkEnteredCount);
 
 
 module.exports = { params };
