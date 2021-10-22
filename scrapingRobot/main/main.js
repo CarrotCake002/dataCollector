d5472f0ca1a344be0455a45c5e7d5d51237cd38e
Pol Siles <pol.siles-polo@epitech.eu>
2021-10-22 13:24:25 +0200

    deleted the getContent function from main.js as i've moved it to a new file. Also deleted the export of params which is being exported in a different file now. main.js consists now simply of a first call to getContent to start the recursivity, and the program will then continue without ever returning to main.js until the end of the program to close it

--- scrapingRobot/main/main.js
+++ scrapingRobot/main/main.js
@@ -1,54 +1,6 @@
-// my imported files
-const imports = require("./imports.js");
+const configStart = require('./../init/config.js');
+const content = require('./getContent.js');
+const init = require('./../init/init.js');
 
-async function getPageData(linkList, iList, page, response) {
-    await imports.click.clickItems(params['clickItems']);
-    var returnArray = await imports.eval.evaluate(linkList, params, iList, page);
-    return (imports.open.checkErrors(returnArray, response.status()) ? returnArray : null);
-}
-
-// main loop of the program. Recursive function that open/closes browsers and gets all the information from every page
-async function getContent(linkList, iList, linkEnteredCount) {
-
-    linkList = await imports.sitemap.check(iList, linkList, params);
-
-    const formattedLink = imports.link.getFormattedLink(iList, linkList[iList][0], params['domain']);
-    linkEnteredCount++;
-
-    // open a new browser and page with the new url
-    var openPage = await imports.open.startPage(params, formattedLink);
-    const [browser, page, response, startTime] = [openPage[0], openPage[1], openPage[2], openPage[3]];
-    openPage = null;
-
-    if (returnArray = await getPageData(linkList, iList, page, response), returnArray === null)
-        return null;
-
-    const endTime = imports.open.endPage(browser, startTime);
-
-    returnArray = returnArray.concat(response.status());
-
-    await imports.save.saveFormData(returnArray, iList, endTime, linkEnteredCount, params);
-
-    var linkList = returnArray[0];
-
-    if (iList = imports.link.getNext(iList, linkList, params), iList === true) {
-        imports.save.end(returnArray, linkEnteredCount, params);
-        return returnArray;
-    }
-
-    returnArray = null;
-    returnArray = await getContent(linkList, iList, linkEnteredCount);
-    return imports.save.end(returnArray, linkEnteredCount, params);
-}
-
-// get the program execution arguments
-const params = imports.configStart.initParams(imports.init.args);
-
-if (params['sitemapLink'] === null)
-    imports.init.linkList = params['startingUrls'];
-
-imports.write.writeInFile('{\n\t', params['savefile']);
-
-getContent(imports.init.linkList, imports.init.iList, imports.init.linkEnteredCount);
-
-module.exports = { params };
+const params = configStart.params;
+content.getContent(configStart.setLinkList(params), init.iList, init.linkEnteredCount);
