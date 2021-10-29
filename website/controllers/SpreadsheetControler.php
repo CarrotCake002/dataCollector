3732f3006b05f95224d4e5f2469dcfcf397ddbd2
Pol Siles <pol.siles-polo@epitech.eu>
2021-10-29 17:53:51 +0200

    added some functions to the spreadsheet controller

--- website/controllers/SpreadsheetControler.php
+++ website/controllers/SpreadsheetControler.php
@@ -7,5 +7,28 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 
 class SpreadsheetController {
-    
+
+    private $sheetName;
+    private $savePath;
+    public $sheet;
+
+    public function __construct($sheetName) {
+        $this->sheetName = $sheetName;
+        $spreadSheet = new Spreadsheet();
+        $this->sheet = $spreadSheet->getActiveSheet();
+    }
+
+    public function setCellValue($cell, $value) {
+        $this->sheet = $this->sheet->setCellValue($cell, $value);
+    }
+
+    public function saveSpreadSheet($path) {
+        $this->savePath = $path;
+        $writer = new Xlsx($this->spreadsheet);
+        $writer->save($path . $this->sheetName . '.xlsx');
+    }
+
+    public function downloadSpreadSheet($path) {
+        header("Location: " . $this->savePath . $this->sheetName . '.xlsx');
+    }
 }
