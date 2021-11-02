1bc97f579953109f9f575693ef7d84260e47f64f
Pol Siles <pol.siles-polo@epitech.eu>
2021-11-02 14:39:56 +0100

    deleted the testing for the new system to delete content from folders in other sessions

--- website/views/openFile.php
+++ website/views/openFile.php
@@ -1,22 +1,5 @@
 <?php
     require_once $_SERVER["DOCUMENT_ROOT"] . '/website/views/header.php';
-
-    $savefilesData = scandir($_SERVER['DOCUMENT_ROOT'] . "/savefiles/");
-
-    for ($i = 0; $i < count($savefilesData); $i++) {
-        if (is_dir($_SERVER['DOCUMENT_ROOT'] . "/savefiles/" . $savefilesData[$i]) && !strpos($_SERVER['DOCUMENT_ROOT'] . "/savefiles/" . $savefilesData[$i], ".")) {
-            echo $savefilesData[$i] . " --> " . date("d/m/y G:i:s", filemtime($_SERVER['DOCUMENT_ROOT'] . "/savefiles/" . $savefilesData[$i]));
-            $dirDate = date_create(date("d-m-y G:i:s", filemtime($_SERVER['DOCUMENT_ROOT'] . "/savefiles/" . $savefilesData[$i])));
-        }
-        echo "<br>";
-    }
-    echo " --> " . date("d/m/y G:i:s");
-    $dateInterval = date_diff(date_create(date("d-m-y G:i:s")), $dirDate);
-    echo "<br>";
-    var_dump($dateInterval);
-    if ($dateInterval->y > 0 || $dateInterval->m > 0 || $dateInterval->d > 0 || $dateInterval->h > 8)
-        echo "<br>bigger";
-    die;
 ?>
 
 <script>
