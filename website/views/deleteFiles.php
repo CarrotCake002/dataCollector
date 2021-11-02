9f432d0ad8b054b66e478b5334ab05eabe61cc59
Pol Siles <pol.siles-polo@epitech.eu>
2021-11-02 14:40:41 +0100

    added the new and functioning system that deletes any folder inside savefiles that hasn't been updated in a certain amount of time

--- website/views/deleteFiles.php
+++ website/views/deleteFiles.php
@@ -7,12 +7,28 @@ require $_SERVER["DOCUMENT_ROOT"] . '/website/controllers/SessionController.php'
 use classes\SessionController;
 
 $session = new SessionController();
-
 $dir = $session->getSessionFolderPath();
 $files = glob($dir . '/' . '*.{json,tmp,xlsx}', GLOB_BRACE);
 
 foreach($files as $file){
     unlink($file);
 }
-
 rmdir($session->getSessionFolderPath());
+
+
+$savefilesData = scandir($_SERVER['DOCUMENT_ROOT'] . "/savefiles/");
+
+for ($i = 0; $i < count($savefilesData); $i++) {
+    $folderPath = $_SERVER['DOCUMENT_ROOT'] . "/savefiles/" . $savefilesData[$i];
+    if (is_dir($folderPath) && !strpos($folderPath, ".")) {
+        $dirDate = date_create(date("d-m-y G:i:s", filemtime($folderPath)));
+        $dateInterval = date_diff(date_create(date("d-m-y G:i:s")), $dirDate);
+        if ($dateInterval->y > 0 || $dateInterval->m > 0 || $dateInterval->d > 0 || $dateInterval->h > 0 || $dateInterval->i > 3) {
+            $files = glob($folderPath . '/*');
+            foreach ($files as $file) {
+                unlink($file);
+            }
+            rmdir($folderPath);
+        }
+    }
+}
