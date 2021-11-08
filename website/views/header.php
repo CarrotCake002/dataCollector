7f5e3bcd021b2177d1f0fef3dab9a15eba78b890
Pol Siles <pol.siles-polo@epitech.eu>
2021-11-08 16:25:30 +0100

    removed a useless '/' in a path

--- website/views/header.php
+++ website/views/header.php
@@ -4,8 +4,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>dataCollector</title>
-    <link rel="stylesheet" type="text/css" href="/style.css">
-    <link rel="stylesheet" type="text/css" href="/switch.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="switch.css">
 </head>
 <body>
     <div id="header_space"><br></div>
