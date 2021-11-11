aaaf183ca7451ad88f3a1c4ac11a9d65e16ca8ee
psiles <pol.siles-polo@epitech.eu>
2021-11-11 14:52:34 +0100

    added a button to the new file in the header list

--- website/views/header.php
+++ website/views/header.php
@@ -14,6 +14,7 @@
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/website/views/openFile.php">Open</a></li>
+                <li><a href="/website/views/showScrapings.php">Files</a></li>
             </ul>
         </nav>
     </header>
