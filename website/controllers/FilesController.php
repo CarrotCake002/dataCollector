77329b6fa708a10bbbacbb4702164cc07509c7fd
psiles <pol.siles-polo@epitech.eu>
2021-11-11 19:03:40 +0100

    all images and other files will be saved there

--- website/controllers/FilesController.php
+++ website/controllers/FilesController.php
@@ -7,4 +7,39 @@ class FilesController {
     private $folder;
     private $file_list;
 
+    public function __construct($folder_name) {
+        $this->folder = $folder_name;
+        $this->file_list = array_values(array_diff(scandir('../../savefiles/' . $folder_name), array('.', '..')));
+    }
+
+    public function getFileListSize() {
+        return count($this->file_list);
+    }
+
+    public function getFileName($fileNb) {
+        if (isset($this->file_list[$fileNb]));
+            return $this->file_list[$fileNb];
+        return false;
+    }
+
+    public function getFileRelativePath($fileNb) {
+        $filename = $this->getFileName($fileNb);
+        if (!$filename)
+            return false;
+        return '../../savefiles/' . $this->folder . '/' . $filename;
+    }
+
+    public function getFileSize($fileNb) {
+        $filedir = $this->getFileRelativePath($fileNb);
+        if (!$filedir)
+            return false;
+        return filesize($filedir) / 1000;
+    }
+
+    public function getFileLastUpdate($fileNb) {
+        $filepath = $this->getFileRelativePath($fileNb);
+        if (!$filepath)
+            return false;
+        return date("d/m/y H:i:s", filemtime($filepath));
+    }
 }
--- website/files/showScrapings.php
+++ website/files/showScrapings.php
@@ -7,23 +7,21 @@ require '../controllers/FilesController.php';
 use classes\SessionController;
 use classes\FilesController;
 
-
-$files = new FilesController();
-
 if (isset($_POST) && isset($_POST['submit']) && isset($_POST['token'])) {
     $session = new SessionController($_POST['token']);
-    if ($session->error)
+    if ($session->error) {
         echo "Error: the token you sent is invalid.<br>If you don't have a valid token, launch the scraping without it and a token will automatically be provided to you.";
         return;
+    }
 } else {
     echo "Error: a problem occured while fetching the data, please try again.";
     return;
 }
 
+$files = new FilesController($session->session_id);
+
 
 ?>
-
-
 
 
 <h1 style="text-align: center; font-size: 48px">Here you can see all your files</h1>
@@ -31,15 +29,22 @@ if (isset($_POST) && isset($_POST['submit']) && isset($_POST['token'])) {
 <table>
     <tr>
         <th>File name</th>
-        <th>status</th>
-        <th>download</th>
+        <th>Size (kB)</th>
+        <th>Last updated</th>
+        <th>Status</th>
+        <th>Download</th>
+        <th>Delete</th>
     </tr>
-    <?php  ?>
+    <?php for ($fileNb = 0; $fileNb < $files->getFileListSize(); $fileNb++): ?>
     <tr>
-        <td>
-
-        </td>
+        <td><?= $files->getFileName($fileNb) ?></td>
+        <td><?= $files->getFileSize($fileNb) ?></td>
+        <td><?= $files->getFileLastUpdate($fileNb) ?></td>
+        <td></td>
+        <td><img src="../../assets/download_button.png" alt="download" style="width: 25px;"></td>
+        <td><img src="../../assets/delete_red_bin.png" alt="delete" style="width: 25px;"></td>
     </tr>
+    <?php endfor; ?>
 </table>
 
 
