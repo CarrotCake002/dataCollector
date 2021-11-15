9b1e50af1be1f21c90fcb2afd0b984ce9da1574c
Pol Siles <pol.siles-polo@epitech.eu>
2021-11-15 14:24:12 +0100

    removed osme empty space that only made the file uglier

--- scrapingRobot/page/open.js
+++ scrapingRobot/page/open.js
@@ -1,12 +1,32 @@
-const puppeteer = require('puppeteer');
+const chromeLauncher = require('chrome-launcher');
+const axios = require('axios');
+const puppeteer = require('puppeteer-core');
 
 // starts the timer, opens new browser and page with the specified parameters and returns all these values
 async function startPage(params, formattedLink) {
     const time = Date.now();
-    const browser = await puppeteer.launch({ headless: params['headlessBrowser'], args: ['--ignore-certificate-errors'] });
+
+    // opens new chrome browser
+    const chrome = await chromeLauncher.launch({
+        chromeFlags: [
+            '--headless',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--no-sandbox'
+        ],
+    });
+
+    // connects puppeteer to the browser's new port
+    const connection = await axios.get(`http://localhost:${chrome.port}/json/version`);
+    const { webSocketDebuggerUrl } = connection.data;
+    var browser = await puppeteer.connect({ browserWSEndpoint: webSocketDebuggerUrl })
+
+    // opens a chrome page
     const page = await browser.newPage();
     await page.setViewport({ width: 1000, height: 926 });
     const response = await page.goto(formattedLink, { waitUntil: 'networkidle0', timeout: 0 });
+
     return [browser, page, response, time];
 }
 
