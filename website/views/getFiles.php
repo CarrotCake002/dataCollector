953be11c438e8a2222b7e40951d6a157e6720ad8
Pol Siles <pol.siles-polo@epitech.eu>
2021-11-19 18:50:18 +0100

    getFiles will now check if the token cookie is set. Before that nothing will be displayed. If the cookie is set it will directly redirect to the files table, if not, it will ask for the token and the user will have to input it

--- website/views/getFiles.php
+++ website/views/getFiles.php
@@ -7,19 +7,43 @@ require_once './header.php';
 <div>
     <div>
         <br>
-        <h2>Get your data with your token</h2><br><br>
+        <h2 class="formBlock" >Get your data with your token</h2><br><br>
     </div>
-    <div>
+    <div class="formBlock">
         <form action="./../files/showScrapings.php" method="post">
-            <p>Token<br>
+            <p >Token<br>
             (Input the token you were given after launching a robot)</p>
-            <input class="home_inputs" type="text" name="token"><br>
-            <input type="submit" name="submit">
+            <?php if (isset($_COOKIE['token'])):?>
+                <input class="home_inputs" type="text" name="token" value="<?=$_COOKIE['token']?>">
+            <?php else: ?>
+                <input class="home_inputs" type="text" name="token"><br>
+            <?php endif; ?>
+            <input type="submit" name="submit" id="getFilesForm">
         </form>
     </div>
 </div>
 
+<script>
+    function submitForm() {
+        document.getElementById('getFilesForm').click();
+    }
+
+    function showForm() {
+        console.log('ola');
+        $(".formBlock").css('display', 'block');
+    }
+</script>
 
 <?php
+
+if (isset($_COOKIE) && isset($_COOKIE['token'])):?>
+    <script>
+        submitForm();
+    </script>
+<?php else: ?>
+    <script>
+        showForm();
+    </script>
+<?php endif;
 
 require_once './footer.php';
