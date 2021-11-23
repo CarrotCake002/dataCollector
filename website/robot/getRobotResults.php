09877fe66aaedd3c1e5440ccf49205dfdfcf286d
Pol Siles <pol.siles-polo@epitech.eu>
2021-11-23 17:31:47 +0100

    added the meta tags with all their data in the CSV file. Since it now works, i'll be looking for ways to add others too

--- website/robot/getRobotResults.php
+++ website/robot/getRobotResults.php
@@ -1,25 +1,22 @@
 <?php
 
-
 require_once './../views/header.php';
 require './../controllers/OpenFileController.php';
 require './../controllers/SessionController.php';
-//require './../controllers/SpreadsheetController.php';
 
 use classes\OpenFileController;
 use classes\SessionController;
-//use classes\SpreadsheetController;
 
 set_time_limit(0);
 
 if (isset($_POST) && isset($_POST['token']) && $_POST['token'] != '') {
     if (!isset($_COOKIE['token']))
-        $session = new SessionController($_POST['token']);
-    if ($session->error) {
+    setcookie('token', $_POST['token'], time() + 28800000, '/');
+    $session = new SessionController($_POST['token']);
+    if ($session === NULL || $session->error) {
         echo 'Error: the token you sent is invalid';
         return;
     }
-    setcookie('token', $_POST['token'], time() + 28800000, '/');
 } else {
     echo "Error: make sure you've set your personal token";
     return;
@@ -55,8 +52,6 @@ if (isset($_FILES)):
         echo "Error: there has been a problem with the file's name.";
         return;
     }
-
-    $sheet = new SpreadsheetController('robotResults');
     $stream = fopen($session->getSessionFolderPath() . '/' . str_replace('.json', '.csv', $_FILES['openFile']['name']), 'w');
 ?>
 
@@ -66,35 +61,33 @@ if (isset($_FILES)):
             <input type="submit" name="submit" value="Get all link details">
         </form>
         <button class="copyTableButton" onclick="copyLinksTable()">Copy Table contents</button>
-        <a href="<?= './../../savefiles/' . $session->getSessionFolderName() . '/' . $sheet->sheetName . '.xlsx' ?>" download="<?= $sheet->sheetName ?>" class="downloadDataExcel" ><br><br>Download an excel with all data!</a>
         <table id="allLinksTable">
             <tr>
-                <th>Id<?= $sheet->setCellValue('A1', 'Id') ?></th>
-                <th>Iteration<?= $sheet->setCellValue('B1', 'Iteration') ?></th>
-                <th>URL<?= $sheet->setCellValue('C1', 'Url') ?></th>
-                <th>Depth<?= $sheet->setCellValue('D1', 'Depth') ?></th>
-                <th>Status<?= $sheet->setCellValue('E1', 'Status') ?></th>
-                <th>Nb links found<?= $sheet->setCellValue('F1', 'Nb links found') ?></th>
+                <th>Id</th>
+                <th>Iteration</th>
+                <th>URL</th>
+                <th>Depth</th>
+                <th>Status</th>
+                <th>Nb links found</th>
                 <th>More details</th>
             </tr>
 
             <?php
-                fputcsv($stream, ['Id',
-                'Iteration',
-                'URL',
-                'Depth',
-                'Times URL found',
-                'Predecessor',
-                'Status',
-                'Load time',
-                'Title', 'Title size',
-                'Nb Meta tags', 'Meta description', 'Meta description size',
-                'Meta tags', 'Meta tags sizes', 'Meta index', 'Meta follow', 'Meta sponsored', 'Meta ugc', 'Meta noopener',
-                'Nb hreflang', 'Hreflang',
-                'Canonical',
-                'Nb links', 'Links', 'Link <a> tags', 'Link target=blank',
-                'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-                'Custom selectors']);
+                fwrite($stream, "'Id'," .
+                "'Iteration'," .
+                "'URL'," .
+                "'Depth'," .
+                "'Times URL found'," .
+                "'Predecessor'," .
+                "'Status'," .
+                "'Load time'," .
+                "'Title'," . "'Title size'," .
+                "'Meta description'," . "'Meta description size'," . "'Nb Meta tags'," . $openFile->getAllMetaTitlesInCSV() .
+                "'Nb hreflang'," . "'Hreflang'," .
+                "'Canonical'," .
+                "'Nb links'," . "'Links'," . "'Link <a> tags'," . "'Link target=blank'," .
+                "'h1'," . "'h2'," . "'h3'," . "'h4'," . "'h5'," . "'h6'," .
+                "'Custom selectors'" . "\n");
                 $objectCount = $openFile->getObjectCount();
 
                 if ($objectCount < 1) {
@@ -104,34 +97,35 @@ if (isset($_FILES)):
                 for ($i = 1; $i <= $objectCount; $i++): ?>
 
             <tr>
-                <td><?= $i; $sheet->setCellValue('A' . $i + 1, $i) ?></td>
-                <td><?= $openFile->getIteration($i); $sheet->setCellValue('B' . $i + 1, $openFile->getIteration($i)) ?></td>
-                <td><a href="<?= $openFile->getURl($i) ?>"><?= $openFile->getUrl($i); $sheet->setCellValue('C' . $i + 1, $openFile->getUrl($i)) ?></a></td>
-                <td><?= $openFile->getUrlDepth($i); $sheet->setCellValue('D' . $i + 1, $openFile->getUrlDepth($i)) ?></td>
-                <td><?= $openFile->getStatus($i); $sheet->setCellValue('E' . $i + 1, $openFile->getStatus($i)) ?></td>
-                <td><?= $openFile->getAllLinksSize($i); $sheet->setCellValue('F' . $i + 1, $openFile->getAllLinksSize($i)) ?></td>
+                <td><?= $i ?></td>
+                <td><?= $openFile->getIteration($i) ?></td>
+                <td><a href="<?= $openFile->getURl($i) ?>"><?= $openFile->getUrl($i) ?></a></td>
+                <td><?= $openFile->getUrlDepth($i) ?></td>
+                <td><?= $openFile->getStatus($i) ?></td>
+                <td><?= $openFile->getAllLinksSize($i) ?></td>
                 <td><a href="<?= '/website/views/getLinkDetails.php/?object=' . $i . '&filename=' . $filePath ?>">Click for more details</a></td>
             </tr>
-            <?php
-                fputcsv($stream,[
-                    $i,
-                    $openFile->getIteration($i),
-                    $openFile->getUrl($i),
-                    $openFile->getUrlDepth($i),
-                    $openFile->getTimesUrlFound($i),
-                    $openFile->getUrlPredecessor($i),
-                    $openFile->getStatus($i),
-                    $openFile->getResponseTime($i),
-                    $openFile->getTitle($i), $openFile->getTitleSize($i),
-                    $openFile->getMetaDescription($i), $openFile->getMetaDescriptionCharSize($i), $openFile->getAllMetaSize($i),
-                    $openFile->getAllMetaInStr($i), $openFile->getAllMetaSizesInStr($i), $openFile->getMetaIndexInStr($i), $openFile->getMetaFollowInStr($i),
-                    $openFile->getMetaSponsoredInStr($i), $openFile->getMetaUgcInStr($i), $openFile->getMetaNoopenerInStr($i), $openFile->getAllHreflangSize($i),
-                    $openFile->getAllHreflangInStr($i), $openFile->getAllCanonicalsInStr($i), $openFile->getAllLinksSize($i), $openFile->getAllLinksInStr($i),
-                    $openFile->getAllLinkArticleInStr($i), $openFile->getAllLinkTargetBlankInStr($i), $openFile->getAllHeadsTypeInStr($i, 0),
-                    $openFile->getAllHeadsTypeInStr($i, 1), $openFile->getAllHeadsTypeInStr($i, 2), $openFile->getAllHeadsTypeInStr($i, 3),
-                    $openFile->getAllHeadsTypeInStr($i, 4), $openFile->getAllHeadsTypeInStr($i, 5), $openFile->getAllUserSelectorsInStr($i)
-                ], ';', '"');
+            <?php                                                                     
+                fwrite($stream,
+                    "'" . $i . "','" .
+                    $openFile->getIteration($i) . "','" .
+                    $openFile->getUrl($i) . "','" .
+                    $openFile->getUrlDepth($i) . "','" .
+                    $openFile->getTimesUrlFound($i) . "','" .
+                    $openFile->getUrlPredecessor($i) . "','" .
+                    $openFile->getStatus($i) . "','" .
+                    $openFile->getResponseTime($i) . "','" .
+                    $openFile->getTitle($i) . "','" . $openFile->getTitleSize($i) . "','" .
+                    $openFile->getMetaDescription($i) . "','" . $openFile->getMetaDescriptionCharSize($i) . "','" . $openFile->getAllMetaSize($i) . "','" .
+                    $openFile->getAllMetaDataInCSV($i)/* . "','" .
+                    $openFile->getAllHreflangSize($i) . "','" .
+                    $openFile->getAllHreflangInStr($i) . "','" . $openFile->getAllCanonicalsInStr($i) . "','" . $openFile->getAllLinksSize($i) . "','" . $openFile->getAllLinksInStr($i) . "','" .
+                    $openFile->getAllLinkArticleInStr($i) . "','" . $openFile->getAllLinkTargetBlankInStr($i) . "','" . $openFile->getAllHeadsTypeInStr($i, 0) . "','" .
+                    $openFile->getAllHeadsTypeInStr($i, 1) . "','" . $openFile->getAllHeadsTypeInStr($i, 2) . "','" . $openFile->getAllHeadsTypeInStr($i, 3) . "','" .
+                    $openFile->getAllHeadsTypeInStr($i, 4) . "','" . $openFile->getAllHeadsTypeInStr($i, 5) . "','" . $openFile->getAllUserSelectorsInStr($i) . "'"
+                */ . "\n");
                 endfor;
+                fclose($stream);
             ?>
         </table>
     </div>
@@ -146,7 +140,6 @@ if (isset($_FILES)):
     </script>
 
 <?php
-$sheet->saveSpreadSheet($session->getSessionFolderPath() . '/');
 endif;
 
 require_once './../views/footer.php';
