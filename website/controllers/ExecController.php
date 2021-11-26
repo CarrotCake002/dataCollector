d5cc30a1ed65fb389f19a558de1a96d6c4e935c7
Pol Siles <pol.siles-polo@epitech.eu>
2021-11-26 18:54:04 +0100

    been looking for errors. The error has been found but no time to fix it today

--- website/controllers/ExecController.php
+++ website/controllers/ExecController.php
@@ -7,16 +7,21 @@ use classes\FilesController;
 class ExecController extends FilesController {
 
     private $folderList;
+    private $tokenFolder;
 
-    public function __construct() {
-        $this->folderList = array_values(array_diff(scandir('../../savefiles/'), array('.', '..')));
+    public function __construct($token) {
+        $this->tokenFolder = $token;
+        $this->folderList = array_values(array_diff(scandir('../savefiles/'), array('.', '..')));
     }
 
     private function getFileListFromFolder($folder) {
-        return array_values(array_diff(scandir('../../savefiles/' . $folder), array('.', '..')));
+        return array_values(array_diff(scandir('../savefiles/' . $folder), array('.', '..')));
     }
 
     private function isFileActive($filepath) {
+        var_dump('Read text in isFileActive in the ExecController my duude');die;
+        // problem here with getFileId, the function is not constructed as expected when creating this.
+        // it needs to be modified or a new one needs to be created in this controller
         return $this->getFileStatus($this->getFileId($filepath)) === 'Active';
     }
 
@@ -26,10 +31,10 @@ class ExecController extends FilesController {
         return $counter;
     }
 
-    private function activeRobotsInFileList($fileList) {
+    private function activeRobotsInFileList($fileList, $folder) {
         $robotCounter = 0;
-
-        foreach ($fileList as $filepath) {
+        foreach ($fileList as $file) {
+            $filepath = '../savefiles/' . $folder . '/' . $file;
             $robotCounter = $this->increaseActiveRobotsCount($robotCounter, $filepath);
         }
         return $robotCounter;
@@ -38,7 +43,7 @@ class ExecController extends FilesController {
     private function getNbExecutingRobotsInFolder($folder) {
         $fileList = $this->getFileListFromFolder($folder);
 
-        return $this->activeRobotsInFileList($fileList);
+        return $this->activeRobotsInFileList($fileList, $folder);
     }
 
     private function getTotalRobotExecutions() {
@@ -54,17 +59,28 @@ class ExecController extends FilesController {
         return $this->getTotalRobotExecutions() > 3;
     }
 
-    public function totalRobotLimitReached() {
-        echo "Info: the simultaneous robots exxecution limit has been reached.<br>
-        You will have to wait for another robot to finish before another one can be launched";
+    private function totalRobotLimitReached() {
+        echo "Info: the simultaneous robots execution limit has been reached.<br>
+        You will have to wait for a robot to finish before another one can be launched.";
+        return true;
+    }
+
+    private function isUserRobotLimitReached() {
+        return $this->getNbExecutingRobotsInFolder($this->tokenFolder) > 1;
+    }
+
+    private function userRobotLimitReached() {
+        echo "Info: the user robots execution limit has been reached.<br>
+        You will have to wait for a robot to finish or manually cancel one before another robot can be launched.";
         return true;
     }
 
     public function isRobotLimitReached() {
+        if ($this->isUserRobotLimitReached())
+        var_dump('die');die;
+            return $this->userRobotLimitReached();
         if ($this->isTotalRobotLimitReached())
             return $this->totalRobotLimitReached();
-        else if ($this->isUserRobotLimitReached())
-            return $this->userRobotLimitReached();
         return false;
     }
 
