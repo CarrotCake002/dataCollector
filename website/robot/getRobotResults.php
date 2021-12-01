4b4763e22c38871f274f7631d8e3fce1eee15751
Pol Siles <pol.siles-polo@epitech.eu>
2021-12-01 18:25:17 +0100

    deleted a commented vardump

--- website/robot/getRobotResults.php
+++ website/robot/getRobotResults.php
@@ -31,7 +31,6 @@ if (isset($_FILES)):
             echo "Error: the token you sent is invalid.<br>If you don't have a valid token, launch the scraping without it and a token will automatically be provided to you.";
             return;
         }
-        //var_dump(file_exists($_FILES['openFile']['tmp_name']));die;
         $filePath = '../../savefiles/' . $session->getSessionFolderName() . '/' . basename($_FILES['openFile']['tmp_name']);
         $move = move_uploaded_file($_FILES['openFile']['tmp_name'], $filePath);
         if ($move === false) {
