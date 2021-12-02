b7cf849c587b252f7a0541f5c8a15d846fbd51a4
Pol Siles <pol.siles-polo@epitech.eu>
2021-12-02 11:25:59 +0100

    changed the title from dataCollector to Robopol, added the footer contents in the header, modified to structure of the header to make the new contents fit, and added emojis to the navigation line to make it more visual and give the website a little more personality

--- website/views/header.php
+++ website/views/header.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>dataCollector</title>
+    <title>Robopol</title>
     <link rel="stylesheet" type="text/css" href="/website/views/style.css">
     <link rel="stylesheet" type="text/css" href="/website/views/switch.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
@@ -11,12 +11,21 @@
 <body>
     <div id="header_space"><br></div>
     <header>
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/website/views/openFile.php">Open</a></li>
-                <li><a href="/website/views/getFiles.php">Files</a></li>
-                <li><a href="/website/token/tokenConfig.php">Token</a></li>
-            </ul>
-        </nav>
+        <div class="profile">
+            <nav>
+                <div style="margin-left: 30px; width: 300px; float: left;">
+                    <a>Brought to you by</a>
+                    <a href="https://github.com/CarrotCake002">CarrotCake002</a>
+                    <a>🥕</a>
+                </div>
+                <div style="margin-right: auto; width: 500px; float: right; right: 3%">
+                    <ul id="nav_list">
+                        <li><a href="/">🏠 Home</a></li>
+                        <li><a href="/website/views/openFile.php">📊 Get data</a></li>
+                        <li><a href="/website/views/getFiles.php">📁 Files</a></li>
+                        <li><a href="/website/token/tokenConfig.php">🔑 Token</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </header>
