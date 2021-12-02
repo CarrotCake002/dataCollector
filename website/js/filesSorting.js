4fa970f771a1e4b4746de0727621b7f55e041a7c
Pol Siles <pol.siles-polo@epitech.eu>
2021-12-02 16:39:52 +0100

    added some of the backend for the filter system. 2 of the functions could be merged into a single one, it would be a great idea to do that

--- website/js/filesSorting.js
+++ website/js/filesSorting.js
@@ -5,17 +5,31 @@ function unclickAll() {
     document.getElementById('filterCsv').checked = false;
 }
 
-function showAll() {
+function showAll(fileList) {
     unclickAll();
     document.getElementById('filterAll').checked = true;
+    for (i = 0; i < fileList.length; i++)
+        document.getElementById('rowFile' + fileList[i]).setAttribute("style", "display: table-row;");
 }
 
-function showJson () {
+function showJson(fileList) {
     unclickAll();
     document.getElementById('filterJson').checked = true;
+    for (i = 0; i < fileList.length; i++) {
+        if (fileList[i].includes('.json'))
+            document.getElementById('rowFile' + fileList[i]).setAttribute("style", "display: table-row;");
+        else
+            document.getElementById('rowFile' + fileList[i]).setAttribute("style", "display: none");
+    }
 }
 
-function showCsv() {
+function showCsv(fileList) {
     unclickAll();
     document.getElementById('filterCsv').checked = true;
+    for (i = 0; i < fileList.length; i++) {
+        if (fileList[i].includes('.csv'))
+            document.getElementById('rowFile' + fileList[i]).setAttribute("style", "display: table-row;");
+        else
+            document.getElementById('rowFile' + fileList[i]).setAttribute("style", "display: none");
+    }
 }
