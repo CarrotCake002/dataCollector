5fd59d44a11e97ca56e78533e851a35a00d8bbba
Pol Siles <pol.siles-polo@epitech.eu>
2021-12-02 11:24:17 +0100

    deleted the text from the footer, will delete the footer itself later

--- website/views/footer.php
+++ website/views/footer.php
@@ -1,9 +1,5 @@
 </body>
 <div id="footer_space"><br></div>
 <footer>
-    <div class="profile">
-        <a>Brought to you by</a>
-        <a href="https://github.com/CarrotCake002">CarrotCake002</a>
-    </div>
 </footer>
 </html>
