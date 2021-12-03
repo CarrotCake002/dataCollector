bc662f1316a60f82814d7dab898a175c93b69e4c
Pol Siles <pol.siles-polo@epitech.eu>
2021-12-03 14:00:58 +0100

    added all the backend to changing the total file size value

--- website/js/filesSorting.js
+++ website/js/filesSorting.js
@@ -5,20 +5,32 @@ function unclickAll() {
     document.getElementById('filter_csv').checked = false;
 }
 
-function showAll(fileList) {
+function showAll(fileList, fileSizeList) {
+    totalSize = 0;
+
     unclickAll();
     document.getElementById('filterAll').checked = true;
-    for (i = 0; i < fileList.length; i++)
+    for (i = 0; i < fileList.length; i++) {
         document.getElementById('rowFile' + fileList[i]).setAttribute("style", "display: table-row;");
+        totalSize += parseFloat(fileSizeList[i]);
+    }
+    console.log(totalSize.toString() + ' kB');
+    document.getElementById('totalFileSize').value = totalSize.toString() + ' kB';
 }
 
-function showFiletype(filetype, fileList) {
-    unclickAll();
+function showFiletype(filetype, fileList, fileSizeList) {
+    totalSize = 0;
+
+    unclickAll();    
     document.getElementById('filter_' + filetype).checked = true;
     for (i = 0; i < fileList.length; i++) {
-        if (fileList[i].includes('.' + filetype))
+        if (fileList[i].includes('.' + filetype)) {
             document.getElementById('rowFile' + fileList[i]).setAttribute("style", "display: table-row;");
+            totalSize += parseFloat(fileSizeList[i]);
+        }
         else
             document.getElementById('rowFile' + fileList[i]).setAttribute("style", "display: none");
     }
+    console.log(totalSize.toString() + ' kB');
+    document.getElementById('totalFileSize').innerHTML = totalSize.toString() + ' kB';
 }
