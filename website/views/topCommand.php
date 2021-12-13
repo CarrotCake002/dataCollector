51fc7c297e79c09a09a935a46d58ddbce7fb62ed
Pol Siles <pol.siles-polo@epitech.eu>
2021-12-13 18:12:41 +0100

    the PID process of a any manually stopped file will now be killed, thus avoiding using ressources

--- website/views/topCommand.php
+++ website/views/topCommand.php
@@ -4,20 +4,14 @@ require './header.php';
 
 exec('ps faux | grep -e node -e chromium', $output);
 
-for ($i = 0; $i < count($output); $i++) {
-    echo htmlentities($output[$i]) . '<br>';
-}
-
-echo '<br><br><br>';
-
 if (isset($_GET) && isset($_GET['filename'])) {
     $filename = pathinfo($_GET['filename'])['filename'];
     if (isset($_COOKIE) && isset($_COOKIE['token'])) {
         $token = $_COOKIE['token'];
         for ($i = 0; $i < count($output); $i++) {
             if (strpos($output[$i], $token) !== false && strpos($output[$i], $filename) !== false) {
-                echo $output[$i] . '<br>';
-                echo $output[$i + 1] .'<br>';
+                exec('kill -9 ' . intval(str_replace('www ', '', $output[$i]))); 
+                exec('kill -9 ' . intval(str_replace('www ', '', $output[$i + 1])));         
             }
         }
     } else {
