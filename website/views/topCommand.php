7d977911156d22886a39b6ac5c5e9e4d53930170
Pol Siles <pol.siles-polo@epitech.eu>
2021-12-13 17:51:54 +0100

    added most code for the topCommand file until now. It's just a test and the name will be changed since it makes no sense now, but I need to make sure it works correctly before continuing

--- website/views/topCommand.php
+++ website/views/topCommand.php
@@ -2,5 +2,31 @@
 
 require './header.php';
 
+exec('ps faux | grep -e node -e chromium', $output);
 
-var_dump($output);
+for ($i = 0; $i < count($output); $i++) {
+    echo htmlentities($output[$i]) . '<br>';
+}
+
+echo '<br><br><br>';
+
+if (isset($_GET) && isset($_GET['filename'])) {
+    $filename = pathinfo($_GET['filename'])['filename'];
+    if (isset($_COOKIE) && isset($_COOKIE['token'])) {
+        $token = $_COOKIE['token'];
+        for ($i = 0; $i < count($output); $i++) {
+            if (strpos($output[$i], $token) !== false && strpos($output[$i], $filename) !== false) {
+                echo $output[$i] . '<br>';
+                echo $output[$i + 1] .'<br>';
+            }
+        }
+    } else {
+        echo 'Error: no token registered.';
+        return;
+    }
+} else {
+    echo 'Error: there has been error processing the file.';
+    return;
+}
+
+echo "Info: the selected robot has successfully been stopped. The changes may take a while to save in the files page.";
