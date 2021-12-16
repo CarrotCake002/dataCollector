97cf061391a705586dc44ff2e764bc75bf260097
Pol Siles <pol.siles-polo@epitech.eu>
2021-12-16 11:47:24 +0100

    removed an intent to create a sleep function in JS because it wouldn't work in my situation. This was to be able to wait some time before reloading the page when creating after creating all CSV files from showScrapings

--- website/js/showScrapings.js
+++ website/js/showScrapings.js
@@ -107,19 +107,15 @@ function showFiletype(filetype, fileList, fileSizeList) {
 }
 
 function generateAllCsvFiles(token, fileList) {
-    document.body.style.cursor = 'wait';
     var csvFilename = "";
+    
     for (i = 0; i < fileList.length; i++) {
-        if (fileList[i].includes(".json") === true) { // also need to check if the file is Finished
+        if (fileList[i].includes(".json") === true) { // also need to check if the file is Finished, or do I?
             csvFilename = fileList[i].replace(".json", ".csv")
             createCsvFromFile(token, csvFilename, fileList[i]);
         }
     }
-    //sleep(10000).then(location.reload());
-}
-
-async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    location.reload();
 }
 
 function createCsvFromFile(token, filename, dataFile) {
